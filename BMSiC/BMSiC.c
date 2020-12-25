b4c73788262764f77f5c12aec96e6e4abf0fa1a3
sebafudi <sebastianfudalej@gmail.com>
2020-12-25 13:18:54 +0100

    Basic menu funcion

--- BMSiC/BMSiC.c
+++ BMSiC/BMSiC.c
@@ -1,11 +1,49 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <curses.h>
+#include <string.h>
+#include <locale.h>
+
+#include <windows.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BMSiC_VERTICAL 0
+#define BMSiC_HORIZONTAL 1
+
+int displayMenu(char *choices[], bool orientation, int yMax, int xMax) {
+	clear();
+	mvprintw(yMax/2, (xMax-strlen(choices[0]))/2, "%s", choices[0]);
+	refresh();
+	return 0;
+}
 
 int main() {
+	char *text[] = { "LOG IN", "SIGN IN" };
+	int yMax, xMax;
+	int choice;
+	int highlight = 0;
+
+	setlocale(LC_CTYPE, "Polish");
+
 	initscr();
-	printw("Hello BMSiC!");
+	noecho();
+	cbreak();
+
+	getmaxyx(stdscr, yMax, xMax);
+	//yMax = 30;
+	//xMax = 60;
+	//wresize(stdscr, yMax, xMax);
+	//resize_term(yMax, xMax);
+	//resize_window(stdscr, yMax, xMax);
+	printw("Hello BMSiC!\n");
+	printw(text[0]);
+	printw("\n%d\n%d\n", yMax, xMax);
 	refresh();
 	getch();
+	displayMenu(text, BMSiC_HORIZONTAL, yMax, xMax);
+	getch();
 	endwin();
+	system("pause");
 	return 0;
 }
