47d9923cf093accde87fd08cc89374457f637e97
sebafudi <sebastianfudalej@gmail.com>
2020-12-26 23:53:37 +0100

    Display center n elements horizontally

--- BMSiC/BMSiC.c
+++ BMSiC/BMSiC.c
@@ -2,6 +2,7 @@
 #include <curses.h>
 #include <string.h>
 #include <locale.h>
+#include <math.h>
 
 #include <windows.h>
 
@@ -11,15 +12,26 @@
 #define BMSiC_VERTICAL 0
 #define BMSiC_HORIZONTAL 1
 
-int displayMenu(char *choices[], bool orientation, int yMax, int xMax) {
+int displayMenu(char **choices, bool orientation, int yMax, int xMax, unsigned int size) {
+	int noOfElements = size / sizeof(choices[0]);
+	int totalTextSize = 0;
+	int cursorLocation = 0;
+	int padding;
+	for (int i = 0; i < noOfElements; i++) {
+		totalTextSize += strlen(choices[i]);
+	}
+	padding = (xMax - totalTextSize) / (noOfElements + 1);
 	clear();
-	mvprintw(yMax/2, (xMax-strlen(choices[0]))/2, "%s", choices[0]);
+	for (int i = 0; i < noOfElements; i++) {
+		mvprintw(yMax / 2, cursorLocation += padding, "%s", choices[i]);
+		cursorLocation += strlen(choices[i]);
+	}
 	refresh();
 	return 0;
 }
 
 int main() {
-	char *text[] = { "LOG IN", "SIGN IN" };
+	char *text[] = { "LOG IN", "SIGN IN"};
 	int yMax, xMax;
 	int choice;
 	int highlight = 0;
@@ -31,17 +43,17 @@ int main() {
 	cbreak();
 
 	getmaxyx(stdscr, yMax, xMax);
-	//yMax = 30;
-	//xMax = 60;
+	yMax = 15;
+	xMax = yMax*4;
 	//wresize(stdscr, yMax, xMax);
-	//resize_term(yMax, xMax);
+	resize_term(yMax, xMax);
 	//resize_window(stdscr, yMax, xMax);
 	printw("Hello BMSiC!\n");
 	printw(text[0]);
 	printw("\n%d\n%d\n", yMax, xMax);
 	refresh();
 	getch();
-	displayMenu(text, BMSiC_HORIZONTAL, yMax, xMax);
+	displayMenu(&text, BMSiC_HORIZONTAL, yMax, xMax, sizeof(text));
 	getch();
 	endwin();
 	system("pause");
