397aa155c3efce4798d7f2a431035ab4f04bc8aa
sebafudi <sebastianfudalej@gmail.com>
2020-12-27 15:05:52 +0100

    Working menu highlight

--- BMSiC/BMSiC.c
+++ BMSiC/BMSiC.c
@@ -14,6 +14,7 @@ int display_horizontal_menu(char** choices, unsigned int size, int yMax,
   int number_of_elements = size / sizeof(choices[0]);
   int total_text_size = 0;
   int cursor_location = 0;
+  int start_location = 0;
   div_t padding;
 
   for (int i = 0; i < number_of_elements; i++) {
@@ -21,30 +22,56 @@ int display_horizontal_menu(char** choices, unsigned int size, int yMax,
   }
   padding = div((xMax - total_text_size), (number_of_elements + 1));
   if (padding.rem > 0) {
-    cursor_location += padding.rem / 2;
+    start_location = padding.rem / 2;
   };
 
   clear();
-  for (int i = 0; i < number_of_elements; i++) {
-    mvprintw(yMax / 2, cursor_location += padding.quot, "%s", choices[i]);
-    cursor_location += strlen(choices[i]);
+  int choice;
+  int highlight = 0;
+  keypad(stdscr, 1);
+  while (1) {
+    cursor_location = start_location;
+    for (int i = 0; i < number_of_elements; i++) {
+      if (i == highlight) wattron(stdscr, A_REVERSE);
+      mvprintw(yMax / 2, cursor_location += padding.quot, "%s", choices[i]);
+      wattroff(stdscr, A_REVERSE);
+      cursor_location += strlen(choices[i]);
+    }
+    choice = wgetch(stdscr);
+    switch (choice) {
+      case KEY_LEFT:
+        highlight--;
+        if (highlight < 0) highlight = 0;
+        break;
+      case KEY_RIGHT:
+        highlight++;
+        if (highlight > number_of_elements - 1)
+          highlight = number_of_elements - 1;
+        break;
+      default:
+        break;
+    }
+    if (choice == 10) {
+      clear(stdscr);
+      refresh();
+      return highlight;
+    }
   }
-  refresh();
-
   return 0;
 }
 
 int main() {
+  char* lang[] = {"ENGLISH", "POLSKI"};
   char* text[] = {"LOG IN", "SIGN IN"};
   int y_max, x_max;
   int choice;
-  int highlight = 0;
 
   setlocale(LC_CTYPE, "Polish");
 
   initscr();
   noecho();
   cbreak();
+  curs_set(0);
 
   getmaxyx(stdscr, y_max, x_max);
   y_max = 15;
@@ -53,11 +80,12 @@ int main() {
   resize_term(y_max, x_max);
   // resize_window(stdscr, yMax, xMax);
   printw("Hello BMSiC!\n");
-  printw(text[0]);
-  printw("\n%d\n%d\n", y_max, x_max);
+  printw("y_max: %d\nx_max: %d\n", y_max, x_max);
   refresh();
   // getch();
-  display_horizontal_menu(&text, sizeof(text), y_max, x_max);
+  choice = display_horizontal_menu(&text, sizeof(text), y_max, x_max);
+  printw("You chose: %d (%s)\n", choice, text[choice]);
+  refresh();
   getch();
   endwin();
   system("pause");
